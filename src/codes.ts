3a22246e82c8f4d6665a83e4a2f6cf6d5dcfa4ab
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2021-09-25 14:50:37 +0200

    fix(codes.ts): missing import

--- src/codes.ts
+++ src/codes.ts
@@ -3,11 +3,13 @@ import { IErrors } from './types';
 // Resources
 import authentication from './resources/authentication';
 import signIn from './resources/signIn';
+import signUp from './resources/signUp';
 import general from './resources/general';
 
 const codes: IErrors = {
   authentication,
   signIn,
+  signUp,
   general,
 };
 
